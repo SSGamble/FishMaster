8fd5aa7cba9b68bc6a2b0d8ef361107cd152fc27
QingRang <970753519@qq.com>
2019-04-23 17:10:09 +0800

    游戏场景搭建
    绘制游戏场景的 UI 界面

---
+++ FishMaster/Assets/Scripts/Effects/Ef_WaterWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 利用切换材质贴图是实现的水波纹特效
+/// </summary>
+public class Ef_WaterWave : MonoBehaviour
+{
+
+    public Texture[] textureArr; // 贴图数组
+    private int index = 0; // 贴图数组的索引
+    private Material material; // 材质
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material; // 获取该物体上的材质
+        InvokeRepeating("ChangeTexture", 0, 0.1f); // 持续切换贴图，达到动画效果
+    }
+
+    void ChangeTexture()
+    {
+        material.mainTexture = textureArr[index]; // 设置当前贴图
+        index = (index + 1) % textureArr.Length; // 循环切换索引，用于切换下一张贴图
+    }
+}
---
+++ FishMaster/Assets/Scripts/GunFollow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 枪跟随鼠标指针移动
+/// </summary>
+public class GunFollow : MonoBehaviour
+{
+
+}
